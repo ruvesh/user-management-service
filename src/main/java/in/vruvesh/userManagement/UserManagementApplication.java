20a05bdced2bdacb5bd47ed3579cd7f5d9cba010
ruvesh <ruvesh2145@gmail.com>
2021-04-15 17:26:58 +0530

    Initial Project setup
    created project structure

---
+++ src/main/java/in/vruvesh/userManagement/UserManagementApplication.java
@@ -0,0 +1,13 @@
+package in.vruvesh.userManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/vruvesh/userManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vruvesh.userManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
